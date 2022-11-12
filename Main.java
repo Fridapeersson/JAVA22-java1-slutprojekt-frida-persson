bd5a822b27c2f251119d8dc014e52d99ba2411e3
Fridapeersson <108325710+Fridapeersson@users.noreply.github.com>
2022-11-12 13:30:32 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,7 @@ package slutprojekt;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.text.DateFormat;
 import javax.swing.JOptionPane;
-
-import slutprojekt.GUI;
 
 public class Main {
 
@@ -28,12 +24,9 @@ public class Main {
 		}
 		
 		JOptionPane.showMessageDialog(null, "Nu kommer kalendern öppnas :)", "Öppna kalender", JOptionPane.WARNING_MESSAGE);
-		
-		
-		
 
-		GUI.createAndPlay();
+
+		GUI.guiCalendar();
 		
 	}
-
 }
